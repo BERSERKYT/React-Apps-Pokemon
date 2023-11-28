f0dcc5c64382e7f3678ea11b54da087317836a36
Danho Guy Sylvestre KRAKOU <gkrakou@praedium-tech.com>
2023-11-28 14:01:13 +0000

    ok

---
+++ src/model/pokemon.ts
@@ -0,0 +1,29 @@
+export default class pokemon {
+    //1. Typage des propriété d'un pokémon.
+    id: number;
+    hp: number;
+    cp: number;
+    name: string;
+    picture: string;
+    types: Array<string>;
+    created: Date;
+
+    //2. Définition des valeur par défaut des propriétés d'un pokémon.
+    constructor(
+        id: number,
+        hp: number = 100,
+        cp: number = 10,
+        name: string = 'name',
+        picture: string = 'http://...',
+        types: Array<string> = ['Normal'],
+        created: Date = new Date()
+    ){
+        this.id = id;
+        this.hp = hp;
+        this.cp = cp;
+        this.name = name;
+        this.picture = picture;
+        this.types = types;
+        this.created = created;
+    }
+}
